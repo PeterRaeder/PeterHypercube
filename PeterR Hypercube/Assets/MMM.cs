961928f6e8db48abb0d6c4930fe3cb4d38d658d5
PeterRaeder <pfraeder5@gmail.com>
2023-12-05 16:40:35 -0800

    More objects, changed speed, advanced menu credits

--- PeterR Hypercube/Assets/MMM.cs
+++ PeterR Hypercube/Assets/MMM.cs
@@ -5,11 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class MMM : MonoBehaviour
 {
+    public GameObject MainPanel;
+    public GameObject CreditsPanel;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        CreditsPanel.SetActive(false);
     }
+public void OpenCredits()
+    {
+        CreditsPanel.SetActive(true);
+        MainPanel.SetActive(false);
+    }
+
+    public void CloseCredits() 
+    {
+        CreditsPanel.SetActive(false);
+        MainPanel.SetActive(true);
+    }
+
+
 public void switchLevel(string LevelName)
     {
         Debug.Log(LevelName);
